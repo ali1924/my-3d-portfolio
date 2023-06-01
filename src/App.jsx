bf649659eb4e9be6dc3be2de159cd24a423f1d60
ali1924 <mohammadali229987@gmail.com>
2023-06-01 20:21:12 +0600

    index.js index.css updated

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import {About,Contact,}
+const App = () => {
+  return (
+    <div>
+       3D Developer Portfolio
+    </div>
+  );
+};
+
+export default App;

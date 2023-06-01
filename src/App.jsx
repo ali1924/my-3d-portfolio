19e1f782371e877c37c6c41aab4c49646e16911f
ali1924 <mohammadali229987@gmail.com>
2023-06-01 20:37:32 +0600

    Browser Router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import {About,Contact,}
+import {Navbar,Hero, About, Experience, Tech, Works, Feedbacks, Contact, StarsCanvas} from './components'
 const App = () => {
   return (
-    <div>
-       3D Developer Portfolio
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar></Navbar>
+          <Hero></Hero>
+        </div>
+        <About></About>
+        <Experience></Experience>
+        <Tech></Tech>
+        <Works></Works>
+        <Feedbacks></Feedbacks>
+        <div className='relative z-0'>
+          <Contact></Contact>
+          <StarsCanvas></StarsCanvas>
+        </div>
     </div>
+    </BrowserRouter>
   );
 };
 
